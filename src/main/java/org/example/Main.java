86c46b1781d2f1de6c76a8b7a2d8e732d4b7d673
ihakalanka <akalankaih19@gmail.com>
2023-01-18 21:53:40 +0530

    created employees array list with data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,19 @@ import java.util.List;
 
 public class Main {
     static List<Employee> employees = new ArrayList<>();
+    static {
+        employees.add(
+                new Employee("Hashan", "Chathuanga", 5000.0)
+        );
+
+        employees.add(
+                new Employee("Naveen", "Fernando", 6000.0)
+        );
+
+        employees.add(
+                new Employee("Nipuna", "Dilshan", 5500.0)
+        );
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
