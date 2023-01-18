8e25ae016375bf4ae680f837b90deeef163fbb36
ihakalanka <akalankaih19@gmail.com>
2023-01-18 21:52:08 +0530

    created employees array list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static List<Employee> employees = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
