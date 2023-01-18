741338ca1620f666b3ef3173910925f79acceb1b
ihakalanka <akalankaih19@gmail.com>
2023-01-18 21:49:22 +0530

    test

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,19 +1,16 @@
 package org.example;
 
-import java.util.List;
-
 public class Employee {
     private String firstName;
     private String lastName;
     private Double salary;
-    private List<String> projects;
 
-    public Employee(String firstName, String lastName, Double salary, List<String> projects) {
+    public Employee(String firstName, String lastName, Double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.salary = salary;
-        this.projects = projects;
     }
+
 
     public String getFirstName() {
         return firstName;
@@ -39,11 +36,12 @@ public class Employee {
         this.salary = salary;
     }
 
-    public List<String> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(List<String> projects) {
-        this.projects = projects;
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
